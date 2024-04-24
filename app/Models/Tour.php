ff86938888b3b784b5aecda99db2269a75d4488e
Engreyxien <engreyxien@gmail.com>
2024-04-24 11:09:33 +0800

    user

--- app/Models/Tour.php
+++ app/Models/Tour.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Country;
+use App\Models\User;
 
 class Tour extends Model
 {
@@ -15,11 +16,17 @@ class Tour extends Model
         'tour_description',
         'tour_price',
         'tour_duration',
-        'country_id'
+        'country_id',
+        'user_id',
     ];
 
     public function Country()
     {
         return $this->belongsTo(Country::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
